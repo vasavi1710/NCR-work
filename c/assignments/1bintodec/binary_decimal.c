d6c091e3401a844da3007e738592412c3728166e
Vasavi <vasavipottupalli@gmail.com>
2019-02-07 21:42:05 +0530

    decimal to binary is also added

--- c/assignments/1bintodec/binary_decimal.c
+++ c/assignments/1bintodec/binary_decimal.c
@@ -1,9 +1,21 @@
 #include<stdio.h>
 #include<math.h>
+int dec_binary(int c);
+int bin_decimal(int b);
 int main()
 {
-	int b, d=0, r=0, i = 0;
-	scanf_s("%d", &b);
+	int bin, dec;
+	scanf_s("%d %d", &bin, &dec);
+	int k=bin_decimal(bin);
+	int l = dec_binary(dec);
+	printf("%d %d", k, l);
+
+	getch();
+	return 0;
+}
+int bin_decimal(int b)
+{
+	int r = 0, d = 0, i = 0;
 	while (b > 0)
 	{
 
@@ -12,7 +24,21 @@ int main()
 		i++;
 		b = b / 10;
 	}
-	printf("%d", d);
-	getch();
-	return 0;
+	return d;
+}
+int dec_binary(int c)
+{
+	int b=0;
+	while (c > 0)
+	{
+		int k;
+		while (c > 0)
+		{
+			k = c % 2;
+			b = (b * 10) + (k);
+			c = c / 2;
+		}
+	}
+	return b;
+
 }
